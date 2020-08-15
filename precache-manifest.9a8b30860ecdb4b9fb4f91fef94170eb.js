ee5c0d81e034531f643ee35cf78409503f08a304
krabuchi <shashu95@yahoo.com>
2020-08-15 12:01:00 +0530

    Updates

---
+++ precache-manifest.9a8b30860ecdb4b9fb4f91fef94170eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e33791e02e7b3531f90bfaf7522dd43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29a730b207deb89a2a73",
+    "url": "/static/css/main.8289fd7b.chunk.css"
+  },
+  {
+    "revision": "7155956c2663cd8916e1",
+    "url": "/static/js/2.4927998c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4927998c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a730b207deb89a2a73",
+    "url": "/static/js/main.435174e1.chunk.js"
+  },
+  {
+    "revision": "58f6e6f8cd910f4f6a8f",
+    "url": "/static/js/runtime-main.262fcfd3.js"
+  },
+  {
+    "revision": "84effdd77e0aeecd1cff901c70a8b6c9",
+    "url": "/static/media/coding-image.84effdd7.jpg"
+  }
+]);
